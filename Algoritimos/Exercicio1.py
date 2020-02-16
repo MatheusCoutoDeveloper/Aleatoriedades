8e34377a0de594feba033bb5a674406fb5e262fa
Matheus Couto <60662588+MatheusCoutoDeveloper@users.noreply.github.com>
2020-02-15 21:07:07 -0300

    Python

---
+++ Algoritimos/Exercicio1.py
@@ -0,0 +1,11 @@
+	a = float (input("Digite o valor de a= "))
+	b = float (input("Digite o valor de b= "))
+	c = float (input("Digite o valor de c= "))
+	delta - b**2-4*a*c
+	print("O valor de delta: ",delta)
+	raizdedelta=delta**0.5
+	print(" raiz de delta: ", raizdodelta)
+	x1=(-b+raizdodelta)/(2*a)
+	print(" x1; ", x1)
+	x2=(-b-raizdodelta)/(2*a)
+	print(" x2: ", x2)
---
+++ Algoritimos/Exercicio2.py
@@ -0,0 +1,6 @@
+a = float(input("Digite o raio do cilindro: "))
+b = float(input("Digite a altura do cilindro: "))
+areadabase = 3.14*r**r
+print ("areadabase)
+volume = h*areadabase
+print("Volume: ", volume)
